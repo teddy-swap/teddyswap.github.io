263af14be33243382acc0d7273c4d970e30deb27
ali-dev-code <ali.asgr514@gmail.com>
2022-12-16 22:02:01 +0500

    social-links

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,77 @@
 		window.setTimeout(fadeout, 0);
 	};
   function fadeout() {
-		document.querySelector(".page-loader").style.opacity = "500";
+		document.querySelector(".page-loader").style.opacity = "0";
 		document.querySelector(".page-loader").style.display = "none";
 	}
+	window.onscroll = function () {
+		var header_navbar = document.querySelector(".navbar-area");
+		var sticky = header_navbar.offsetTop;
+		if (window.pageYOffset > sticky) {
+			header_navbar.classList.add("sticky");
+		} else {
+			header_navbar.classList.remove("sticky");
+		}
+		var backToTo = document.querySelector(".scroll-top");
+		if (
+			document.body.scrollTop > 50 ||
+			document.documentElement.scrollTop > 50
+		) {
+			backToTo.style.display = "flex";
+		} else {
+			backToTo.style.display = "none";
+		}
+	};
+
+	var pageLink = document.querySelectorAll(".page-scroll");
+	pageLink.forEach((elem) => {
+		elem.addEventListener("click", (e) => {
+			e.preventDefault();
+			document
+				.querySelector(elem.getAttribute("href"))
+				.scrollIntoView({ behavior: "smooth", offsetTop: 1 - 60 });
+		});
+	});
+	function onScroll(event) {
+		var sections = document.querySelectorAll(".page-scroll");
+		var scrollPos =
+			window.pageYOffset ||
+			document.documentElement.scrollTop ||
+			document.body.scrollTop;
+		for (var i = 0; i < sections.length; i++) {
+			var currLink = sections[i];
+			var val = currLink.getAttribute("href");
+			var refElement = document.querySelector(val);
+			var scrollTopMinus = scrollPos + 73;
+			if (
+				refElement.offsetTop <= scrollTopMinus &&
+				refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+			) {
+				document
+					.querySelector(".page-scroll")
+					.classList.remove("active");
+				currLink.classList.add("active");
+			} else {
+				currLink.classList.remove("active");
+			}
+		}
+	}
+	window.document.addEventListener("scroll", onScroll);
+	let navbarToggler = document.querySelector(".navbar-toggler");
+	var navbarCollapse = document.querySelector(".navbar-collapse");
+	document.querySelectorAll(".page-scroll").forEach((e) =>
+		e.addEventListener("click", () => {
+			navbarToggler.classList.remove("active");
+			navbarCollapse.classList.remove("show");
+		})
+	);
+	navbarToggler.addEventListener("click", function () {
+		navbarToggler.classList.toggle("active");
+	});
+
+
+
+
 
 
 })();
